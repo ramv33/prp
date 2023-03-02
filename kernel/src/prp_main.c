3c2af491c37115cc6d505c426737c690674663b0
Abhiram V <abhi.raa.man.v@gmail.com>
2023-03-02 11:40:41 +0530

    Rename module init and exit functions

--- kernel/src/prp_main.c
+++ kernel/src/prp_main.c
@@ -1,18 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-static int __init prp_init(void)
+static int __init prp_module_init(void)
 {
 	printk(KERN_INFO "Loading PRP\n");
 	return 0;
 }
 
-static void __exit prp_exit(void)
+static void __exit prp_module_exit(void)
 {
 	printk(KERN_INFO "Unloading PRP\n");
 }
 
 MODULE_LICENSE("GPL");
 
-module_init(prp_init);
-module_exit(prp_exit);
+module_init(prp_module_init);
+module_exit(prp_module_exit);
