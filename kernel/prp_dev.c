4cc37a2d2f83a86a88b1c256bd550bf5bb73a39a
Abhiram V <abhi.raa.man.v@gmail.com>
2023-03-18 21:58:24 +0530

    prp_dev: Do some net_device setup in prp_dev_setup
    Define device_type structure with name = "prp".
    Define some skeleton net_device_ops (TODO: complete them).
    Might have to check up on the dev->hw_features and dev->features later.

--- kernel/prp_dev.c
+++ kernel/prp_dev.c
@@ -1,8 +1,86 @@
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+#include "prp_main.h"
 #include "prp_dev.h"
+#include "debug.h"
 
+static struct device_type prp_type = {
+	.name = "prp"
+};
+
+static int prp_dev_change_mtu(struct net_device *net, int mtu)
+{
+	/* TODO: Ensure it does not exceed max MTU of either slave */
+	PDEBUG("[PRP] prp_dev_change_mtu to %d\n", mtu);
+	return 0;
+}
+
+/* Called when network device transitions to the UP state */
+static int prp_dev_open(struct net_device *dev)
+{
+	/* TODO */
+	PDEBUG("[PRP] prp_dev_open\n");
+	return 0;
+}
+
+/* Called when network device transitions to the DOWN state */
+static int prp_dev_close(struct net_device *dev)
+{
+	/* Nothing to do here I think */
+	PDEBUG("[PRP] prp_dev_close\n");
+	return 0;
+}
+
+/* Transmit packet */
+static netdev_tx_t prp_dev_xmit(struct sk_buff *skb, struct net_device *dev)
+{
+	struct prp_dev *prp = netdev_priv(dev);
+
+	/*
+	 * TODO:
+	 * 	Call another function that adds the RCT and forwards
+	 */
+	PDEBUG("[PRP] prp_dev_xmit\n");
+	return NETDEV_TX_OK;
+}
+
+/*
+ * Adjust requested feature flags and return the resulting flags.
+ * Must not modify the device state
+ */
+// static netdev_features_t prp_fix_features(struct net_device *dev,
+// 						netdev_features_t features)
+// {
+// 	return 0;
+// }
+
+static const struct net_device_ops prp_device_ops = {
+	.ndo_change_mtu = prp_dev_change_mtu,
+	.ndo_open = prp_dev_open,
+	.ndo_stop = prp_dev_close,
+	.ndo_start_xmit = prp_dev_xmit,
+	// .ndo_fix_features = prp_fix_features,
+};
+
+/* Called from rtnl_link_ops. */
 void prp_dev_setup(struct net_device *dev)
 {
-	/* TODO
-	 * Use ether_setup(dev) too
-	 */
+	eth_hw_addr_random(dev);
+	ether_setup(dev);
+	dev->min_mtu = 0;
+	// dev->header_ops = NULL;
+	dev->netdev_ops = &prp_device_ops;
+
+	SET_NETDEV_DEVTYPE(dev, &prp_type);
+	dev->hw_features = NETIF_F_SG		/* Scatter/gather IO */
+			| NETIF_F_FRAGLIST 	/* Scatter/gather IO */
+			| NETIF_F_HIGHDMA	/* Can DMA to high memory */
+			| NETIF_F_GSO_MASK 	/* Segmentation offload feature mask */
+			| NETIF_F_HW_CSUM 	/* Can checksum all packets */
+			;
+	dev->features = dev->hw_features;
+	/* cannot handle VLAN */
+	dev->features |= NETIF_F_VLAN_CHALLENGED;
+	PDEBUG("prp_dev_setup done\n");
+}
 }
