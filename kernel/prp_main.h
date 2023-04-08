51742b7976713396e305a2433446a4c47810f5c3
Abhiram V <abhi.raa.man.v@gmail.com>
2023-04-08 16:42:55 +0530

    Define prp_rct

--- kernel/prp_main.h
+++ kernel/prp_main.h
@@ -1,6 +1,23 @@
 #ifndef PRP_MAIN_H
 #define PRP_MAIN_H
 
+/**
+ * PRP Redundancy Control Trailer (RCT) as specified in IEC 62439-3:2016 (p. 20)
+ * Appended to frames.
+ *
+ * @seqnr:			16-bit sequence number
+ * @lan_id_and_lsdu_size:	4-bit LAN ID and 12-bit frame size
+ * 				LSDU size is the size of the Ethernet payload
+ * 				including the RCT, upto and excluding the FCS
+ * @prp_suffix:			16-bit PRP suffix (0x88FB)
+ */
+struct prp_rct {
+	__be16	seqnr;
+	/* 4-bit LAN identifier and 12-bit frame size */
+	__be16	lan_id_and_lsdu_size;
+	__be16	prp_suffix;
+} __packed ;
+
 struct prp_port {
 	struct net_device	*dev;
 	struct prp_priv		*master;
