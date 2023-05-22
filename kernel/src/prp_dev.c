0e20872d7e8b8a4f85742a83f0133c525e57aeeb
Abhiram V <abhi.raa.man.v@gmail.com>
2023-05-22 17:26:33 +0530

    Clean up main

--- kernel/src/prp_dev.c
+++
@@ -1,8 +0,0 @@
-#include "prp_dev.h"
-
-void prp_dev_setup(struct net_device *dev)
-{
-	/* TODO
-	 * Use ether_setup(dev) too
-	 */
-}
--- kernel/src/prp_dev.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PRP_DEV_H
-#define __PRP_DEV_H
-
-#include <linux/netdevice.h>
-
-void prp_dev_setup(struct net_device *dev);
-
-#endif /* __PRP_DEV_H */
